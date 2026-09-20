a3f80fe66ae522081043b3b088b1af78a88f7860
Azkaban734 <Azkaban734@users.noreply.github.com>
2026-09-20 13:37:24 -0400

    fix: change middleware to fetch trailing slash instead of index.html to prevent Cloudflare 308 redirect

--- functions/internal/schedule/_middleware.js
+++ functions/internal/schedule/_middleware.js
@@ -2,7 +2,7 @@ export const onRequest = async (context) => {
   const response = await context.next();
   if (response.status === 404) {
     const url = new URL(context.request.url);
-    url.pathname = '/internal/schedule/index.html';
+    url.pathname = '/internal/schedule/';
     return context.env.ASSETS.fetch(new Request(url, context.request));
   }
   return response;
--- functions/internal/warehouse/_middleware.js
+++ functions/internal/warehouse/_middleware.js
@@ -2,7 +2,7 @@ export const onRequest = async (context) => {
   const response = await context.next();
   if (response.status === 404) {
     const url = new URL(context.request.url);
-    url.pathname = '/internal/warehouse/index.html';
+    url.pathname = '/internal/warehouse/';
     return context.env.ASSETS.fetch(new Request(url, context.request));
   }
   return response;
